7b06a17b57bdc6bd2189b93751e7308092efb5e5
sortofsleepy <sortofsleepy@gmail.com>
2021-08-17 18:18:49 -0700

    working on getting recording based drawing working

--- src/Sketcher.ts
+++ src/Sketcher.ts
@@ -1,5 +1,34 @@
 
 
+class DrawInstance {
+    x:number = 0;
+    y:number = 0;
+    lastX:number = 0;
+    lastY:number = 0;
+    opacity:number = 1;
+
+    constructor(x,y) {
+
+        this.x = x;
+        this.y = y;
+
+    }
+
+    render(ctx){
+        ctx.beginPath();
+        ctx.moveTo(this.lastX,this.lastY);
+        ctx.lineTo(this.x,this.y);
+        ctx.stroke();
+        ctx.closePath();
+
+        // helps fill in the gaps since lineTo creates breaks with larger widths.
+        ctx.beginPath();
+        ctx.arc(this.x,this.y,ctx.lineWidth / 2,0,2 * Math.PI)
+        ctx.fill();
+        ctx.closePath();
+    }
+}
+
 export default class Sketcher {
 
     el:HTMLCanvasElement;
@@ -14,7 +43,8 @@ export default class Sketcher {
         isDown:false
     }
 
-    frameCount:number = 0;
+    drawings:Array<DrawInstance> = [];
+
     constructor(el:HTMLCanvasElement) {
 
         this.el = el;
@@ -34,9 +64,71 @@ export default class Sketcher {
 
     start(){
 
+        let render = () => {
+            requestAnimationFrame(render);
+            this._applyDrawSettings();
+
+
+            if(this.mouse.isDown){
+                this.drawings.forEach((draw,i) => {
+                    draw.render(this.ctx);
+                });
+            }
+
+        };
+
+        this.animateId = requestAnimationFrame(render);
+    }
+
+    _applyDrawSettings(){
+
+        let lineWidth = window["settings"].lineWidth;
+        let fillStyle = window["settings"].fillStyle;
+        this.ctx.lineWidth = lineWidth;
+        this.ctx.fillStyle = fillStyle;
+        this.ctx.strokeStyle = window["settings"].strokeStyle;
     }
 
     _setupMouseListeners(){
+
+        window.addEventListener("pointerdown",()=>{
+            this.mouse.isDown = true;
+
+        });
+        window.addEventListener("pointermove",()=>{
+
+            let x = this.mouse.x ;
+            let y = this.mouse.y;
+
+            if(this.mouse.isDown){
+                this.drawings.push(new DrawInstance(x,y));
+                let lastLastSet = this.drawings[this.drawings.length - 2];
+                let lastSet = this.drawings[this.drawings.length - 1];
+
+
+                if(lastLastSet !== undefined){
+                    lastSet.lastX = lastLastSet.x;
+                    lastSet.lastY = lastLastSet.y;
+                }
+
+            }
+
+        });
+
+        window.addEventListener("pointerup",()=>{
+            this.mouse.isDown = false;
+
+
+            /*
+             // get the last drawing set and set the last x,y pos
+            let lastSet = this.drawings[this.drawings.length - 1];
+            lastSet.lastX = this.mouse.x;
+            lastSet.lastY = this.mouse.y;
+            this.drawings[this.drawings.length - 1] = lastSet;
+             */
+
+
+        });
 
     }
 }
